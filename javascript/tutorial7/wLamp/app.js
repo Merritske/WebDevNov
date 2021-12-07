93a0a2b6a6239edcd3df87990febb798bab0767d
merritske <merritske@gmail.com>
2021-12-07 17:57:55 +0100

    update lamp

--- javascript/tutorial7/wLamp/app.js
+++ javascript/tutorial7/wLamp/app.js
@@ -1,23 +1,26 @@
 
-var lamp = document.getElementById("lamp off")
+
 
 let control = document.getElementById('control')
 let light_control = false;
 let lamp = document.getElementById('lamp')
+//let lamp2 = document.getElementById('lamp2')
 let jumbo = document.getElementById('jumbo')
+let title = document.getElementById('title')
 
 function lightSwitch(){
     if(!light_control){// true
         light_control= true
         console.log('Lamp is on!!!!')
-       control.src= 
-        lamp.src='https://toppng.com//public/uploads/preview/lamp-vector-idea-street-light-115628923387naqgyphbv.png" alt="" style="width:20%'
+     title.style.color = "white"
+        lamp.src='yellow-light-bulb-11533048327xi88wag0zp.png'
         jumbo.style.background='black'
+        
     }else{
         light_control=false
         console.log('Lamp is off!!!!')
-        
-        lamp.src='https://toppng.com//public/uploads/preview/lamp-vector-idea-street-light-115628923387naqgyphbv.png" alt="" style="width:20%'
+    title.style.color = "black"
+    lamp.src='png-transparent-light-bulb-illustration-incandescent-light-bulb-lamp-bulb-candle-product-light-thumbnail.png'
         jumbo.style.background='#e9ecef'
     }
 }
