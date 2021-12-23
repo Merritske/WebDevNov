ac26c15d2fe9c88ad2345caec38516e1d8dc6d46
merritske <merritske@gmail.com>
2021-12-23 16:15:27 +0100

    update23

--- javascript/tutorial13/inputform.js
+++ javascript/tutorial13/inputform.js
@@ -3,9 +3,19 @@ const [title,director,coverLink] = document.querySelectorAll("input")
 let list = []
 let tableBody = document.getElementById("films")
 
+
+let clearAll = document.getElementById("clear-films")
+clearAll.addEventListener("click", clearbtn=>{
+    tableBody.innerHTML =""
+    localStorage.removeItem("list")
+})
+
+
 function testRemove(getEl){
 getEl.parentElement.parentElement.remove()
 console.log(getEl.parentElement.parentElement)
+
+
 }
 
 addBtn.addEventListener("click", addInput)
@@ -16,6 +26,8 @@ let obj = {
     director : director.value,
     coverLink : coverLink.value
 }
+//zie onderaan voor andere oplossing
+
 list.push(obj)
 
 
@@ -30,12 +42,12 @@ localStorage.setItem("list", JSON.stringify(list))
 
 }
 
+
 window.addEventListener('DOMContentLoaded', (event)=>{
     let listNew = JSON.parse(localStorage.getItem("list"))
     
     console.log(listNew)
-  
-      
+        
     listNew.forEach(movieList =>{
       
  tableBody.innerHTML +=`<tr>
@@ -46,8 +58,17 @@ window.addEventListener('DOMContentLoaded', (event)=>{
  </tr>`
    list= listNew  })
     
-
 })
 
-let clearAll = document.getElementById("clear-films")
+//andere oplossing => window.addEventListener kan hiermee weg
+// let allmovies = JSON.parse(localStorage.getItem('Addtable-data'))
+//         allmovies.push(obj)
+//         localStorage.setItem('Addtable-data',JSON.stringify(allmovies))
+
+//         table.innerHTML += `<td>${obj.movie}</td>
+//         <td>${obj.director}</td>
+//         <td>${obj.link}</td>
+//         <td><a href="#" id = "delete-film" class = "btn btn-danger" onClick="TestRemove(this)">Delete Movie</a></td>`
+        
+//     })
 
